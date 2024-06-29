0679412fa38ff6c3ce29dc0557404b5c2d04c1ef
podisto <omardione1991@gmail.com>
2024-06-28 22:15:30 -0400

    init front ms

---
+++ ms-front/src/main/java/com/medilabo/MsFrontApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFrontApplication.class, args);
+	}
+
+}
