f53b9c0f89d6b7b9b4a7d0012e283c5e4829a146
podisto <omardione1991@gmail.com>
2024-06-29 19:43:55 -0400

    sprint 02: expose endpoint to add note

--- ms-note/src/main/java/com/medilabo/resource/NoteController.java
+++ ms-note/src/main/java/com/medilabo/resource/NoteController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -40,5 +37,17 @@ public class NoteController {
         }
 
         return ResponseEntity.ok(optional.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<Note> addNote(@RequestBody NoteRequest noteRequest) {
+        log.info("attempt to add note for Patient ID {}, name {}", noteRequest.getPatId(), noteRequest.getPatient());
+        Note note = Note.builder()
+                .patId(noteRequest.getPatId())
+                .patient(noteRequest.getPatient())
+                .notes(noteRequest.getNotes())
+                .build();
+        Note created = noteRepo.save(note);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 }
---
+++ ms-note/src/main/java/com/medilabo/resource/NoteRequest.java
@@ -0,0 +1,13 @@
+package com.medilabo.resource;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class NoteRequest {
+    private String patId;
+    private String patient;
+    private List<String> notes = new ArrayList<>();
+}
