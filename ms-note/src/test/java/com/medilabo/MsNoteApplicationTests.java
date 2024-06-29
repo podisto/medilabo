e7c9e126b9d9f66be638724c0b54d8c4be0e0d32
podisto <omardione1991@gmail.com>
2024-06-29 18:34:26 -0400

    sprint 01 DONE

---
+++ ms-note/src/test/java/com/medilabo/MsNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
