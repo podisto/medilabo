49483952a8d4da002048119037a5fdd5c57dd0e6
podisto <omardione1991@gmail.com>
2024-06-30 06:03:07 -0400

    add documentation + fix initial data in patient ms getting duplicated

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-# Demarrage de l'application medilabo
+# Documentation de l'application medilabo
 
+#### 1. Lancement
 Pour lancer l'application il vous faudra docker installe sur votre machine.
 
-`docker-compose up`
+- Au premier lancement vous pouvez lancer la commande 
+`docker-compose up -d`
+
+- Si vous apportez des modifications sur le code, pour redeployer vos changements lancer la commande
 
 ` docker-compose up --force-recreate --build -d`
+
+#### 2. Les composantes
+
+L'application est constituee des microservices (ms) suivants:
+
+- la gateway 8080
+- le ms front (UI) 8081
+- le ms patient 8082
+- le ms note 8083
+- le ms authentication 8085
--- patient.sql
+++ patient.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS patient;
+
 create table if not exists patient(
     id bigint not null auto_increment,
     birth_date date not null,
@@ -10,12 +12,14 @@ create table if not exists patient(
 ) engine=InnoDB;
 
 
-TRUNCATE TABLE patient;
+INSERT INTO patient (last_name, first_name, birth_date, gender, address, phone_number)
+VALUES ('TestBorderline', 'Test', '19450624', 'M', '2 High St', '200-333-4444');
 
-INSERT INTO patient (last_name, first_name, birth_date, gender, address, phone_number) VALUES ('TestBorderline', 'Test', '19450624', 'M', '2 High St', '200-333-4444');
 INSERT INTO patient (last_name, first_name, birth_date, gender, address, phone_number)
 VALUES ('TestNone', 'Test', '19661231', 'F', '1 Brookside St', '100-222-3333');
+
 INSERT INTO patient (last_name, first_name, birth_date, gender, address, phone_number)
 VALUES ('TestInDanger', 'Test', '20040618', 'M', '3 Club Road', '300-444-5555');
+
 INSERT INTO patient (last_name, first_name, birth_date, gender, address, phone_number)
 VALUES ('TestEarlyOnset', 'Test', '20020628', 'F', '4 Valley Dr', '400-555-6666');
